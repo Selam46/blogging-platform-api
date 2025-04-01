07eee849399ea9980c960541ebb7950acf0ed3a9
Selam <selamawitmesfin55@gmail.com>
2025-04-01 04:43:21 -0700

    Implement post likes, view tracking, post status

--- blogging_platform/blog/models.py
+++ blogging_platform/blog/models.py
@@ -1,26 +1,89 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.text import slugify
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True, blank=True)
+    description = models.TextField(blank=True)
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
+    
+    class Meta:
+        verbose_name_plural = 'Categories'
 
 class Tag(models.Model):
     name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True, blank=True)
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
 
 class Post(models.Model):
+    STATUS_CHOICES = [
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    ]
+    
     title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True, blank=True)
     content = models.TextField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    excerpt = models.TextField(blank=True, help_text="A short description of the post")
+    featured_image = models.URLField(blank=True, help_text="URL to the featured image")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, related_name='posts')
     published_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
     created_date = models.DateTimeField(auto_now=True)
-    tags = models.ManyToManyField(Tag, blank=True)
+    tags = models.ManyToManyField(Tag, blank=True, related_name='posts')
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+    read_time = models.PositiveIntegerField(default=0, help_text="Estimated read time in minutes")
+    views = models.PositiveIntegerField(default=0)
+    likes = models.ManyToManyField(User, blank=True, related_name='liked_posts')
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        
+        # Calculate read time if not provided
+        if not self.read_time:
+            word_count = len(self.content.split())
+            self.read_time = max(1, round(word_count / 200))  # Assuming 200 words per minute
+            
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.title
+        
+    @property
+    def like_count(self):
+        return self.likes.count()
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='replies')
+    content = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    is_approved = models.BooleanField(default=True)
+    
+    class Meta:
+        ordering = ['-created_date']
+    
+    def __str__(self):
+        return f'Comment by {self.author.username} on {self.post.title}'
+        
+    @property
+    def is_reply(self):
+        return self.parent is not None
--- blogging_platform/blog/serializers.py
+++ blogging_platform/blog/serializers.py
@@ -1,17 +1,78 @@
 from rest_framework import serializers
-from .models import Post, Category, Tag
+from .models import Post, Category, Tag, Comment
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'first_name', 'last_name']
 
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tag
-        fields = '__all__'
+        fields = ['id', 'name', 'slug']
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = '__all__'
+        fields = ['id', 'name', 'slug', 'description']
+
+class CommentSerializer(serializers.ModelSerializer):
+    author_detail = UserSerializer(source='author', read_only=True)
+    replies = serializers.SerializerMethodField()
+    
+    class Meta:
+        model = Comment
+        fields = ['id', 'post', 'author', 'author_detail', 'parent', 'content', 
+                 'created_date', 'is_approved', 'replies']
+        read_only_fields = ['author', 'is_approved']
+    
+    def get_replies(self, obj):
+        if obj.parent is None:  # Only get replies for top-level comments
+            replies = Comment.objects.filter(parent=obj)
+            return CommentSerializer(replies, many=True).data
+        return []
+    
+    def create(self, validated_data):
+        validated_data['author'] = self.context['request'].user
+        return super().create(validated_data)
 
 class PostSerializer(serializers.ModelSerializer):
+    author = serializers.ReadOnlyField(source='author.username')
+    author_detail = UserSerializer(source='author', read_only=True)
+    category_detail = CategorySerializer(source='category', read_only=True)
+    tags_detail = TagSerializer(source='tags', many=True, read_only=True)
+    like_count = serializers.ReadOnlyField()
+    comments = CommentSerializer(many=True, read_only=True)
+    tags = serializers.PrimaryKeyRelatedField(queryset=Tag.objects.all(), many=True, required=False)
+    
     class Meta:
         model = Post
-        fields = '__all__'
+        fields = ['id', 'title', 'slug', 'content', 'excerpt', 'featured_image', 
+                 'author', 'author_detail', 'category', 'category_detail', 
+                 'tags', 'tags_detail', 'published_date', 'updated_date', 
+                 'created_date', 'status', 'read_time', 'views', 'like_count', 'comments']
+        read_only_fields = ['slug', 'read_time', 'views', 'like_count']
+        
+    def create(self, validated_data):
+        tags_data = validated_data.pop('tags', [])
+        post = Post.objects.create(**validated_data)
+        
+        for tag in tags_data:
+            post.tags.add(tag)
+            
+        return post
+        
+    def update(self, instance, validated_data):
+        tags_data = validated_data.pop('tags', None)
+        
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        
+        if tags_data is not None:
+            instance.tags.clear()
+            for tag in tags_data:
+                instance.tags.add(tag)
+                
+        instance.save()
+        return instance

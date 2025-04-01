3f55ee8ad88ae4a4e4ea8a2d3517fb92df4c31bd
Selam <selamawitmesfin55@gmail.com>
2025-04-01 04:41:04 -0700

    Add permission system

--- blogging_platform/blog/views.py
+++ blogging_platform/blog/views.py
@@ -1,20 +1,125 @@
-from django.shortcuts import render
-from rest_framework import viewsets
-from .models import Post, Category, Tag
-from .serializers import PostSerializer, CategorySerializer, TagSerializer
+from django.shortcuts import render, get_object_or_404
+from rest_framework import viewsets, filters, status
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Post, Category, Tag, Comment
+from .serializers import PostSerializer, CategorySerializer, TagSerializer, CommentSerializer
 from django.http import HttpResponse
+from .permissions import IsAuthorOrReadOnly
+from .filters import PostFilter
+from django.utils.text import slugify
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_class = PostFilter
+    search_fields = ['title', 'content', 'author__username', 'category__name', 'tags__name']
+    ordering_fields = ['published_date', 'created_date', 'title', 'views', 'read_time']
+    ordering = ['-published_date']
+    lookup_field = 'slug'
+    
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+    
+    def create(self, request, *args, **kwargs):
+        # Handle string tags if provided
+        data = request.data.copy()
+        tag_names = data.pop('tag_names', None)
+        
+        # Process the normal serializer validation
+        serializer = self.get_serializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        
+        # Save the post
+        post = serializer.save(author=self.request.user)
+        
+        # Process tags if provided as strings
+        if tag_names:
+            if isinstance(tag_names, str):
+                tag_names = [tag.strip() for tag in tag_names.split(',')]
+                
+            for tag_name in tag_names:
+                tag, created = Tag.objects.get_or_create(
+                    name=tag_name,
+                    defaults={'slug': slugify(tag_name)}
+                )
+                post.tags.add(tag)
+        
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        # Increment view count
+        instance.views += 1
+        instance.save()
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
+    
+    @action(detail=True, methods=['post'], permission_classes=[IsAuthenticated])
+    def like(self, request, slug=None):
+        post = self.get_object()
+        user = request.user
+        
+        if post.likes.filter(id=user.id).exists():
+            post.likes.remove(user)
+            return Response({'status': 'unliked'}, status=status.HTTP_200_OK)
+        else:
+            post.likes.add(user)
+            return Response({'status': 'liked'}, status=status.HTTP_200_OK)
+    
+    @action(detail=False, methods=['get'])
+    def drafts(self, request):
+        if request.user.is_authenticated:
+            posts = Post.objects.filter(author=request.user, status='draft')
+            page = self.paginate_queryset(posts)
+            if page is not None:
+                serializer = self.get_serializer(page, many=True)
+                return self.get_paginated_response(serializer.data)
+            serializer = self.get_serializer(posts, many=True)
+            return Response(serializer.data)
+        return Response({'error': 'You must be authenticated'}, status=status.HTTP_401_UNAUTHORIZED)
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    lookup_field = 'slug'
 
 class TagViewSet(viewsets.ModelViewSet):
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
+    lookup_field = 'slug'
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.filter(parent=None)  # Only top-level comments
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
+    
+    def get_queryset(self):
+        queryset = Comment.objects.filter(parent=None)
+        post_id = self.request.query_params.get('post', None)
+        if post_id is not None:
+            queryset = queryset.filter(post_id=post_id)
+        return queryset
+    
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+    
+    @action(detail=True, methods=['post'], permission_classes=[IsAuthenticated])
+    def reply(self, request, pk=None):
+        parent_comment = self.get_object()
+        data = request.data.copy()
+        data['post'] = parent_comment.post.id
+        data['parent'] = parent_comment.id
+        
+        serializer = CommentSerializer(data=data, context={'request': request})
+        if serializer.is_valid():
+            serializer.save(author=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 def home(request):
     return HttpResponse("Welcome to the Blogging Platform API!")
